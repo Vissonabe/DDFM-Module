7a258476eab09ea909fdf9c406d171ef0d48d59f
vissonabe <vissonabe@gmail.com>
2020-10-15 15:49:34 +0530

    integrated lint checks

--- deeplink/src/main/java/com/viswa/deeplink/DeeplinkHandler.kt
+++ deeplink/src/main/java/com/viswa/deeplink/DeeplinkHandler.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class DeeplinkHandler constructor(private val processors: Set<@JvmSuppressWildcards IDeeplinkProcessor>) : IDeeplinkHandler {
+class DeeplinkHandler constructor(private val processors: Set<@JvmSuppressWildcards IDeeplinkProcessor>)
+    : IDeeplinkHandler {
     override fun process(intent: Intent, activity: AppCompatActivity?, shouldRender: Boolean): Bundle? {
         processors.forEach {
-            if(it.matches(intent)){
-                val processedIntent =  it.process(intent)
+            if (it.matches(intent)) {
+                val processedIntent = it.process(intent)
                 if (shouldRender) {
                     activity?.let { activity -> it.renderTarget(processedIntent, activity) }
                 }
